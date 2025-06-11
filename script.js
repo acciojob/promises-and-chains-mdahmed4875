7618fa0fbe8368c570ccd8ce81cb95d3785cf38c
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-06-11 10:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let form=document.getElementById("form");
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	const name = nameInput.value.trim();
+    const age = parseInt(ageInput.value.trim());
+	if(name.value==""||age.value==null){
+		alert("Please enter valid details.");
+		return;
+	}
+	const validation=new promise((res,rej)=>{
+	setTimeout(()=>{
+		if(age>18){
+			res(name);
+		}
+		else{
+			rej(name);
+		}
+	},4000)
+	validation.then((uname)=>{
+		alert(`Welcome, ${uname}. You can vote.`);
+	}).catch(()=>{
+		alert(`Oh sorry ${uname}. You aren't old enough.`);
+	})
+})
+})
